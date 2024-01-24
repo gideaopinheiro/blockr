06ac35fed65bc1b02107755beea02c609b33d173
gideaopinheiro <gpss@ic.ufal.br>
2024-01-24 10:04:16 -0300

    update point with new fn

---
+++ test/point_test.exs
@@ -0,0 +1,20 @@
+defmodule PointTest do
+  use ExUnit.Case
+  doctest Point
+
+  test "New Point" do
+    assert Point.new(1, 1) == {1, 1}
+  end
+
+  test "Move point down" do
+    assert Point.move_down({1, 1}) == {2, 1}
+  end
+
+  test "Move point left" do
+    assert Point.move_left({1, 1}) == {1, 0}
+  end
+
+  test "Move point right" do
+    assert Point.move_right({1, 1}) == {1, 2}
+  end
+end
